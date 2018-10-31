9f16be6d999e32496bbd277a8dc10eb296c3e0ac
3A532092 <star1998410@gmail.com>
2018-10-31 23:55:53 +0800

    練習九 產生StudentController.原本路由中的閉包換成控制器

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,15 @@ Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject
 })->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);*/
 
 
-/*Route::group(['prefix'=>'student'],function(){
+Route::group(['prefix'=>'student'],function(){
     Route::get('{student_no}',[
             'as'=>'student',
-            'uses'=>function($student_no){
-            return '學號:'.$student_no;
-        }
+            'uses'=>'StudentController@getStudentData'
+        
     ]);
     Route::get('{student_no}/score/{subject?}',[
         'as'=>'student.score',
-        'uses'=>function($student_no,$subject = null){
-            return '學號:'.$student_no.'  的'.((is_null($subject))?'所有科目':$subject).'成績';
-        }
+        'uses'=>'StudentController@getStudentScore'
     ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
     
-});*/
+});

b609f2e7999ed25b722c3a587f0c1b7ae28e69c4
3A532092 <star1998410@gmail.com>
2018-10-31 23:29:25 +0800

    練習七 路由命名

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,11 +17,17 @@ Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject
 
 
 Route::group(['prefix'=>'student'],function(){
-    Route::get('{student_no}',function($student_no){
-        return "學號:".$student_no;
-    });
-    Route::get('{student_no}/score/{subject?}',function($student_no,$subject = null){
-        return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
-    })->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
+    Route::get('{student_no}',[
+            'as'=>'student',
+            'uses'=>function($student_no){
+            return '學號:'.$student_no;
+        }
+    ]);
+    Route::get('{student_no}/score/{subject?}',[
+        'as'=>'student.score',
+        'uses'=>function($student_no,$subject = null){
+            return '學號:'.$student_no.'  的'.((is_null($subject))?'所有科目':$subject).'成績';
+        }
+    ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
     
 });

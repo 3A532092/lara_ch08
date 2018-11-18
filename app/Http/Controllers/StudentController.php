e391550cce92285230689f309734de214657a504
3A532092 <star1998410@gmail.com>
2018-11-18 21:39:35 +0800

    練習五：DB存取 & Model練習part II

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+
+use App\Student;
+
+use View;
+
 class StudentController extends Controller
 {
     //
@@ -14,7 +20,7 @@ class StudentController extends Controller
         return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
     }*/
 
-    public function getStudentData($student_no){
+    /*public function getStudentData($student_no){
         return \View::make('score',[
             'student_no'=>$student_no,
             'subject'=>null
@@ -23,6 +29,25 @@ class StudentController extends Controller
     public function getStudentScore($student_no,$subject = null){
         return \View::make('score',[
             'student_no'=>$student_no,
+            'subject'=>$subject
+        ]);
+    }*/
+
+    public function getStudentData($student_no){
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
+            'subject'=>null
+        ]);
+    }
+    public function getStudentScore($student_no,$subject = null){
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
             'subject'=>$subject
         ]);
     }
